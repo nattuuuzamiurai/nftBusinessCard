995055e2130fad75bed7ef8f440fd5472621687a
nattuuuzamurai <87963027+nattuuuzamiurai@users.noreply.github.com>
2022-11-02 02:25:52 +0900

    isAddress 分岐

--- full-mint-website/src/NavBar.js
+++ full-mint-website/src/NavBar.js
@@ -2,20 +2,7 @@ import React from "react";
 import { Button, Box, Flex, Image, Link, Spacer } from "@chakra-ui/react";
 import Twitter from "./assets/social-media-icons/logo.png";
 
-const NavBar = ({ accounts, setAccounts }) => {
-	const isConnected = Boolean(accounts[0]);
-
-	async function connectAccount() {
-		if (window.ethereum) {
-			const accounts = await window.ethereum.request({
-				//このメソッドでメタマスクに存在するアカウントを全て渡してくれる
-				method: "eth_requestAccounts",
-			});
-			//アカウントをアップデート
-			setAccounts(accounts);
-		}
-	}
-
+const NavBar = () => {
 	return (
 		<Flex justify="space-between" align="center" padding="30px">
 			{}
@@ -25,25 +12,6 @@ const NavBar = ({ accounts, setAccounts }) => {
 				</Link>
 			</Flex>
 			{}
-			{isConnected ? (
-				<Box margin="0 15px">
-					Connected:{accounts[0].substr(0, 4)}...{accounts[0].substr(-4, 4)}
-				</Box>
-			) : (
-				<Button
-					backgroundColor="#D6517D"
-					borderRadius="5px"
-					boxShadow="0px 2px 2px 1px #0F0F0F"
-					color="white"
-					cursor="pointer"
-					fontFamily="inherit"
-					padding="15px"
-					margin="0 15px"
-					onClick={connectAccount}
-				>
-					Connect
-				</Button>
-			)}
 		</Flex>
 	);
 };
