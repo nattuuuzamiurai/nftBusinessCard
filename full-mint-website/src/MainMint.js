ea66abc3e913800bee9802ad91deab23228e8aae
nattuuuzamurai <87963027+nattuuuzamiurai@users.noreply.github.com>
2022-11-01 16:44:15 +0900

    チュートリアル終了

--- full-mint-website/src/MainMint.js
+++ full-mint-website/src/MainMint.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { ethers, BigNumber } from "ethers";
+import { Button, Box, Flex, Input, Text } from "@chakra-ui/react";
 import BusinessCardNft from "./BusinessCardNft.json";
 
 const BusinessCardNftAddress = process.env.NFT_ADDRESS;
 
 const MainMint = ({ accounts, setAccounts }) => {
 	//ミントの数を定義。デフォルトは１
-	const [mintAmount, setMintAmount] = useState(1);
 	const isConnected = Boolean(accounts[0]);
 
 	async function handleMint() {
@@ -28,17 +28,38 @@ const MainMint = ({ accounts, setAccounts }) => {
 			}
 		}
 	}
-
 	return (
 		<div>
-			<h1>NFT名刺</h1>
-			<p>なっとう侍の名刺NFTをゲットしよう! mint費用がガス代のみ!</p>
+			<Text fontSize="48px" textShadow="0 5px #000000">
+				NFT名刺
+			</Text>
+			<Text
+				fontSize="30px"
+				letterSpacing="-5.5%"
+				fontFamily="VT323"
+				textShadow="0 2px 2px #000000"
+			>
+				なっとう侍の名刺NFTをゲットしよう! mint費用がガス代のみ!
+			</Text>
 			{isConnected ? (
 				<div>
-					<button onClick={handleMint}>Mint Now</button>
+					<Flex align="center" justify="center"></Flex>
+					<Button
+						backgroundColor="#D6517D"
+						borderRadius="5px"
+						boxShadow="0px 2px 2px 1px #0F0F0F"
+						color="white"
+						cursor="pointer"
+						fontFamily="inherit"
+						padding="15px"
+						marginTop="10px"
+						onClick={handleMint}
+					>
+						Mint Now
+					</Button>
 				</div>
 			) : (
-				<p>You mast be connected to Mint.</p>
+				<p>You must be connected to Mint.</p>
 			)}
 		</div>
 	);
--- full-mint-website/src/NavBar.js
+++ full-mint-website/src/NavBar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button, Box, Flex, Image, Link, Spacer } from "@chakra-ui/react";
+import Twitter from "./assets/social-media-icons/logo.png";
 
 const NavBar = ({ accounts, setAccounts }) => {
 	const isConnected = Boolean(accounts[0]);
@@ -15,18 +17,34 @@ const NavBar = ({ accounts, setAccounts }) => {
 	}
 
 	return (
-		<div>
+		<Flex justify="space-between" align="center" padding="30px">
 			{}
-			<div>Twitter</div>
-			{}
-
+			<Flex justify="space-around" width="40%" padding="0 75px">
+				<Link href="https://twitter.com/nattuuu_zamurai">
+					<Image src={Twitter} boxSize="84px" margin="0 15px" />
+				</Link>
+			</Flex>
 			{}
 			{isConnected ? (
-				<p>Connected</p>
+				<Box margin="0 15px">
+					Connected:{accounts[0].substr(0, 4)}...{accounts[0].substr(-4, 4)}
+				</Box>
 			) : (
-				<button onClick={connectAccount}>Connect</button>
+				<Button
+					backgroundColor="#D6517D"
+					borderRadius="5px"
+					boxShadow="0px 2px 2px 1px #0F0F0F"
+					color="white"
+					cursor="pointer"
+					fontFamily="inherit"
+					padding="15px"
+					margin="0 15px"
+					onClick={connectAccount}
+				>
+					Connect
+				</Button>
 			)}
-		</div>
+		</Flex>
 	);
 };
 
