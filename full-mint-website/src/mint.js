53ee1745fc3c0dd280b7283773dcb972dbb82e1a
nattuuuzamurai <87963027+nattuuuzamiurai@users.noreply.github.com>
2022-11-04 15:34:38 +0900

    console.log

--- full-mint-website/src/mint.js
+++ full-mint-website/src/mint.js
@@ -3,6 +3,8 @@ import { getNftAddress, getRpc } from "./keys/lib.js";
 import { ethers } from "ethers";
 import BusinessCardNft from "./BusinessCardNft.json";
 
+console.log(getNftAddress);
+
 const mint = async (recipient) => {
 	const provider = new ethers.providers.JsonRpcProvider(getRpc);
 	const contract = new ethers.Contract(
